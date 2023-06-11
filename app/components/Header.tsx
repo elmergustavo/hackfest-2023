0105cd06df98da0bca417b9ca2bc23a894339b33
fernando <ferrafterminador@gmail.com>
2023-06-10 23:14:17 -0600

    creating base project

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <main className="flex justify-center my-[17px]">
+            <p>
+                Colitas logo
+            </p>
+        </main>
+    )
+}
